bc1c891f99bf5b7ea21d4ca3eca2832c33facccf
Adarsh Dubey <adarshdubeydelhi@gmail.com>
2025-01-03 00:15:03 +0530

    AWS CONFIGURATION FOR S3 BUCKET

--- BACKEND/config/aws-config.js
+++ BACKEND/config/aws-config.js
@@ -1,1 +1,11 @@
 // WE WRITE ALL THE CONFIGURATION ABOUT THE CONNECTION BETWEEN THE CLOUD MEANS AWS 
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({region:"eu-north-1"});
+
+const s3 = new AWS.S3();
+
+const S3_BUCKET = "sampleadarshbucket";
+
+module.exports = {s3 , S3_BUCKET}
